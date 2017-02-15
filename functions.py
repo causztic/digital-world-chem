6061399c4de3e7a2258f85458ed34e38b131eada
yaojie <limyaojie93@gmail.com>
2017-02-15 23:57:57 +0800

    + legendre

--- functions.py
+++ functions.py
@@ -6,6 +6,7 @@ Created on Sun Feb  5 11:48:13 2017
 @author: yaojie
 """
 import scipy.constants as c
+from scipy import special
 import numpy as np
 
 # week 2
@@ -42,9 +43,80 @@ def cartesian_to_spherical(x,y,z):
 ########
 
 # week 3
+
+# 4
 def fact(f):
     result = 1
     while f > 1:
         result *= f
         f -= 1
     return result
+    
+# 5
+
+#def p00(theta):
+#	return 1
+#
+#def p01(theta):
+#	return np.cos(theta)
+#
+#def p02(theta):
+#	return 0.5*(3*np.cos(theta)**2-1)
+#
+#def p03(theta):
+#	return 0.5*(5*np.cos(theta)**3-3*np.cos(theta))
+#
+#def p11(theta):
+#	return np.sin(theta)
+#
+#def p12(theta):
+#	return 3*np.sin(theta)*np.cos(theta)
+#
+#def p13(theta):
+#	return 1.5*np.sin(theta)*(5*np.cos(theta)**2-1)
+#
+#def p22(theta):
+#	return 3*np.sin(theta)**2
+#
+#def p23(theta):
+#	return 15*np.sin(theta)**2*np.cos(theta)
+#
+#def p33(theta):
+#	return 15*np.sin(theta)*(1-np.cos(theta)**2)
+#
+#def assoc_legendre(m,l):
+#	if m==0 and l==0:
+#		return p00
+#	elif m==0 and l==2:
+#		return p02
+#	elif m==1 and l==1:
+#		return p11
+#	elif m==3 and l==3:
+#		return p33
+#	elif m==0 and l==1:
+#		return p01
+#	elif m==2 and l==3:
+#		return p23
+#	elif m==2 and l==2:
+#		return p22
+#	elif m==1 and l==3:
+#		return p13
+#	elif m==1 and l==2:
+#		return p12
+#	elif m==0 and l==3:
+#		return p03
+#	else:
+#		return None
+
+# cheat answer
+def assoc_legendre(m, l):
+    return lambda theta: abs(special.lpmn(m,l,np.cos(theta))[0][m][l])
+
+# 6
+def assoc_laguerre(q, p, x):
+    return special.assoc_laguerre(x, p,q-p)
+    
+print assoc_laguerre(0,0,1)
+print assoc_laguerre(1,1,1)
+print assoc_laguerre(2,2,1)
+print assoc_laguerre(2,2,0)
