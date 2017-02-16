e9b12482556778fbd80474c2ad9ff362cb4a9f54
yaojie <limyaojie93@gmail.com>
2017-02-16 09:32:59 +0800

    + laguerre

--- functions.py
+++ functions.py
@@ -8,6 +8,7 @@ Created on Sun Feb  5 11:48:13 2017
 import scipy.constants as c
 from scipy import special
 import numpy as np
+import math
 
 # week 2
 
@@ -112,11 +113,91 @@ def fact(f):
 def assoc_legendre(m, l):
     return lambda theta: abs(special.lpmn(m,l,np.cos(theta))[0][m][l])
 
-# 6 #not working yet
+# 6
+#def l00(x):
+#	return 1
+#
+#def l01(x):
+#	return -x+1
+#
+#def l02(x):
+#	return x*x-4*x+2
+#
+#def l10(x):
+#	return 1
+#
+#def l11(x):
+#	return -2*x+4
+#
+#def l12(x):
+#	return 3*x*x-18*x+18
+#
+#def l13(x):
+#    return -4*x*x*x+48*x*x-144*x+96
+#
+#def l20(x):
+#	return 2
+#
+#def l21(x):
+#	return -6*x+18
+#
+#def l23(x):
+#    return -20*x*x*x+300*x*x-1200*x+1200
+#
+#def l22(x):
+#	return 12*x*x-96*x+144
+#
+#def l03(x):
+#    return -x*x*x+9*x*x-18*x+6
+#
+#def l30(x):
+#	return 6
+#
+#def l31(x):
+#	return -24*x+96
+#
+#def l32(x):
+#	return 60*x*x-600*x+1200
+#
+#def assoc_laguerre(p,qmp):
+#	if p==0 and qmp==0:
+#		return l00
+#	elif p==0 and qmp==1:
+#		return l01
+#	elif p==0 and qmp==2:
+#		return l02
+#	elif p==0 and qmp==3:
+#		return l03
+#	elif p==1 and qmp==0:
+#		return l10
+#	elif p==1 and qmp==1:
+#		return l11
+#	elif p==1 and qmp==2:
+#		return l12
+#	elif p==1 and qmp==3:
+#		return l13
+#	elif p==2 and qmp==0:
+#		return l20
+#	elif p==2 and qmp==1:
+#		return l21
+#	elif p==2 and qmp==2:
+#		return l22
+#	elif p==2 and qmp==3:
+#		return l23
+#	elif p==3 and qmp==0:
+#		return l30
+#	elif p==3 and qmp==1:
+#		return l31
+#	elif p==3 and qmp==2:
+#		return l32
+#	elif p==3 and qmp==3:
+#		return l33
+#	else:
+#		return None
+  
 def assoc_laguerre(p, qmp):
-    return lambda x: special.eval_genlaguerre(qmp, p, x)
-    
-print assoc_laguerre(0,0)(1) #1
-print assoc_laguerre(1,1)(1) #2
-print assoc_laguerre(2,2)(1) #60
-print assoc_laguerre(2,2)(0) #144
+    return lambda x: int(math.factorial(p+qmp)*special.eval_genlaguerre(qmp, p, x))
+
+for i in range(0, 4):
+    for j in range(0,4):
+        print i,j, assoc_laguerre(i,j)(1)
